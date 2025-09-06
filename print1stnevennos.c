793898220b31372e5e9bfd8942000dad2cdc9e13
Jeel Solanki <jeelsolanki953@gmail.com>
2025-09-06 17:52:03 +0530

    Add files via upload

---
+++ print1stnevennos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i, n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("First %d even numbers are:\n", n);
+    for(i = 2; i <= 2*n; i += 2) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
