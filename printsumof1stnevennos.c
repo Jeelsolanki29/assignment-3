264dc89c2035376a74ef60f2a29edae3e389ef23
Jeel Solanki <jeelsolanki953@gmail.com>
2025-09-06 18:00:32 +0530

    Add files via upload

---
+++ printsumof1stnevennos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i, n, sum = 0;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= 2*n; i += 2) {
+        sum += i;
+    }
+    printf("sum of first %d even numbers = %d\n", n, sum);
+
+    return 0;
+}
